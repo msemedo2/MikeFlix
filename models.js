6f01923af8e2c879cf5f1de293d0665a724eab70
unknown <miguel.semedo.2@outlook.com>
2022-06-22 22:14:26 +0100

    create mongoose schema

---
+++ models.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+	Title: { type: String, required: true },
+	Description: { type: String, required: true },
+	Genre: {
+		Name: String,
+		Description: String,
+	},
+	Director: {
+		Name: String,
+		Bio: String,
+	},
+	ImagePath: String,
+	Featured: Boolean,
+});
+let userSchema = mongoose.Schema({
+	Username: { type: String, required: true },
+	Password: { type: String, required: true },
+	email: { type: String, required: true },
+	BirthDate: Date,
+	FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
