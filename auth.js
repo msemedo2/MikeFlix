dc7b6abe981e942d3da4bc61923633cfdc5b0051
unknown <miguel.semedo.2@outlook.com>
2022-06-28 20:18:21 +0100

    create endpoint - HTTP authentication for login requests

---
+++ auth.js
@@ -0,0 +1,35 @@
+const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+	passport = require('passport');
+
+require('./passport.js'); // local passport file
+
+let generateJWTToken = (user) => {
+	return jwt.sign(user, jwtSecret, {
+		subject: user.Username, // username encoded in the JWT
+		expiresIn: '7d', // This specifies that the token will expire in 7 days
+		algorithm: 'HS256', // This is the algorithm used to “sign” or encode the values of the JWT
+	});
+};
+
+// Post login
+module.exports = (router) => {
+	router.post('/login', (req, res) => {
+		passport.authenticate('local', { session: false }, (error, user, info) => {
+			if (error || !user) {
+				return res.status(400).json({
+					message: 'Something is not right',
+					user,
+				});
+			}
+			req.login(user, { session: false }, (error) => {
+				if (error) {
+					res.send(error);
+				}
+				let token = generateJWTToken(user.toJSON());
+				return res.json({ user, token });
+			});
+		})(req, res);
+	});
+};
