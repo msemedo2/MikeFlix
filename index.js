dbdabf9994d3a633df666e1d6787581966246156
unknown <miguel.semedo.2@outlook.com>
2022-06-29 18:19:03 +0100

    connect to Heroku

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/MikeFlix', {
+// mongoose.connect('mongodb://localhost:27017/MikeFlix', {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// });
+mongoose.connect('process.env.CONNECTION_URI', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
