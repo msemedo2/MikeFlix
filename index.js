ee70a0b81db98fe301f48fe04dacae8e1d086882
unknown <miguel.semedo.2@outlook.com>
2022-06-29 19:12:25 +0100

    update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { check, validationResult } = require('express-validator');
 // 	useNewUrlParser: true,
 // 	useUnifiedTopology: true,
 // });
-mongoose.connect('process.env.CONNECTION_URI', {
+mongoose.connect(process.env.CONNECTION_URI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
