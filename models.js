8989612a7b1fb7f2d6705b92b8f73d1bde42eb11
unknown <miguel.semedo.2@outlook.com>
2022-06-29 14:24:29 +0100

    add password hash

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
 	Title: { type: String, required: true },
@@ -18,9 +19,17 @@ let userSchema = mongoose.Schema({
 	Username: { type: String, required: true },
 	Password: { type: String, required: true },
 	email: { type: String, required: true },
-	BirthDate: Date,
+	Birthday: Date,
 	FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
+
+userSchema.statics.hashPassword = (password) => {
+	return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function (password) {
+	return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
