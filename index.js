e59c0e4842ffab5eb7372295c0d03cc8c1179db6
unknown <miguel.semedo.2@outlook.com>
2022-07-16 15:07:48 +0100

    add authentication logic in movies endpoint

--- index.js
+++ index.js
@@ -97,30 +97,20 @@ app.get(
 );
 
 //GET all movies
-app.get('/movies', function (req, res) {
-	Movies.find()
-		.then(function (movies) {
-			res.status(201).json(movies);
-		})
-		.catch(function (error) {
-			console.error(error);
-			res.status(500).send('Error: ' + error);
-		});
-});
-// app.get(
-// 	'/movies',
-// 	passport.authenticate('jwt', { session: false }),
-// 	(req, res) => {
-// 		Movies.find()
-// 			.then((movies) => {
-// 				res.status(201).json(movies);
-// 			})
-// 			.catch((error) => {
-// 				console.error(error);
-// 				res.status(500).send('Error: ' + error);
-// 			});
-// 	}
-// );
+app.get(
+	'/movies',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		Movies.find()
+			.then((movies) => {
+				res.status(201).json(movies);
+			})
+			.catch((error) => {
+				console.error(error);
+				res.status(500).send('Error: ' + error);
+			});
+	}
+);
 
 //GET user by Username
 app.get(
